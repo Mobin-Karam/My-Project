3f0e32e2ae7d54b071cbe9da4c608ad8151d4b81
mobinkaram <polyt.mobin.karam@gmail.com>
2023-02-07 14:28:38 +0330

    3 commit

--- Javascript-course/class-1/index.js
+++ Javascript-course/class-1/index.js
@@ -1,18 +1,84 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN');
+// let js = 'amazing';
+// if (js === 'amazing') alert('Javascript is FUN');
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
-
-
-let age = 40;
-age = 30;
-
-const birthday = 40;
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
 
 
-let firstName = 'robbin';
+// let age = 40;
+// age = 30;
 
-console.log(firstName);
- 
-const figiNa = 'ljflasel kjpwiq lkijqew km lkzjl fjiej '
+
+// const birthday = 40;
+
+
+// let firstName = 'robbin';
+
+// console.log(firstName);
+
+// const figiNa = 'ljflasel kjpwiq lkijqew km lkzjl fjiej '
+
+// const country = "iran";
+// const continent = "Asia";
+// const population = "eightyMillion";
+
+// console.log(country,continent ,population );
+
+// const country = "IRAN";
+// const continent = "Asia";
+// let population = "80million";
+// let isIsland = false;
+// const language = "Persian";
+// population = "70million";
+
+// isIsland = "khar";
+
+
+// console.log(typeof country, continent, population, isIsland, language);
+
+// ----LECTURE: Values and Variables
+// let country = 'Portugal';
+// let continent = 'Europe';
+// let population = 10;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// ----LECTURE: Data Types
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+// -----LECTURE: let, const and var
+// language = 'portuguese';
+// const country = 'Portugal';
+// const continent = 'Europe';
+// const isIsland = false;
+// isIsland = true
+
+// const numberOne = 2;
+// const numberTwo = 6;
+
+// let totalNumber = numberOne + numberTwo;
+
+// totalNumber += "3";
+
+// let totalNumber = numberOne + numberTwo;
+
+// console.log(totalNumber);
+
+
+// const carName = "Audi";
+
+// document.getElementById("demo").innerHTML = carName;
+
+// let carName;
+
+// var carName = "BMW"
+// var carName;
+
+
+
+
+
