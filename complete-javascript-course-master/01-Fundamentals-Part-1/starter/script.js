8c77d9b00c068950825c4a0ee486303411c1c2ae
Mobin-Karam <polyt.mobin.karam@gmail.com>
2023-02-24 01:33:51 +0330

    01

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,160 @@
+//^--->  Lecture : Hello World 
+//^ ------------------------
+
+//&-NOTE-1  >>> We have three way to open up the chrome developer tools :
+//&----- : First = Ctrl + Shift + I , Second = Open with inspect , Third = open with Chrome control > moretools > Developers tools , and open with F12
+
+
+//&--NOTE-2  >>> We can also go to the pervios commands code in the console with up arrow just like that .
+
+//&---NOTE-3 >>> 
+
+//~---> Start of the coding ::::
+
+
+// alert("Hello world");
+
+// let js = 'amazing';
+
+// if (js === 'amazing') alert('Javascript is FUN');
+
+// js = 'boring';
+
+// if (js === 'amazing') alert('Javascript is FUN');
+
+
+//^--->  Lecture : A Breif Introduction of the Javascript 
+//^ ------------------------
+
+
+
+//&-NOTE-1  >>> 
+
+
+
+//~---> Start of the coding ::::
+
+
+
+
+
+
+
+//^--->  Lecture : JS Strings and Template literals
+//^ ------------------------
+
+
+
+//&-NOTE-1  >
+
+
+/*
+*
+  *
+  *
+  *
+  *
+  *
+  *
+  *
+*/
+
+//~---> Start of the coding ::::
+
+
+// console.log("string text line 1\n" + "string text line 2");
+// "string text line 1
+// string text line 2"
+
+
+// const firstName = 'Mobin';
+// const job = 'Jonior Front end developer'
+// const birthYear = 2002;
+// const year = 2023;
+
+// const mobin = "I'm " + firstName + ", a " + (year - birthYear) + 'years old ' + job + '!';
+
+// console.log(mobin);
+
+// const mobinNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+
+// console.log(mobinNew);
+
+// console.log(`Hello I'm
+// Mobin
+// Karam
+// from another country's`)
+
+
+
+
+//^--->  Lecture : JS Taking Decision if else Statements
+//^ ------------------------
+
+
+
+//&-NOTE-1  >
+
+
+/*
+*
+  *
+  *
+  *
+  *
+  *
+  *
+  *
+*/
+
+//~---> Start of the coding ::::
+
+
+const age = 21;
+const toEndOfLife = age >= 30;
+
+if (toEndOfLife) { //if () that mean if in the parantez is true every things in the brakets its Run .
+  console.log('Hello my name is MobinKaram.')
+}
+
+
+
+
+
+
+
+
+
+
+
+//^----> Challeng number One -----------
+
+
+
+
+
+ //* ----> Data 1 :
+//---- mark Height and Weight -----
+// const markHeight = 1.69;
+// const markMass = 78;
+//---- john Height and Weight -----
+// const johnHeight = 1.95;
+// const johnMass = 92;
+//! ----> Data 2 :
+//---- mark Height and Weight -----
+// const markHeight = 1.88;
+// const markMass = 95;
+//---- john Height and Weight -----
+// const johnHeight = 1.76;
+// const johnMass = 85;
+
+
+
+// const markBMI = markMass / (markHeight ** 2);
+// const johnBMI = johnMass / (johnHeight ** 2);
+
+//?------ make Boolien ------
+// const markHigherBMI = (johnBMI < markBMI);
+
+
+// console.log(johnBMI, markBMI, markHigherBMI);
---
+++ ordering-robot.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=options)
+
+driver.get("https://food.razi.ac.ir/")
+driver.maximize_window()
+
+uname = driver.find_element("id", "username")
+uname.send_keys("4019503020")
+upass = driver.find_element("id", "password")
+upass.send_keys("3220219039")
+driver.find_element(By.TAG_NAME, "button").click()
+
+links = driver.find_elements(
+    "xpath", "//ul[contains(@class, 'sidebar-menu tree')][.//li[contains(@class, 'treeview')]][..//a[text()[contains(., رزرو غذا)]]]")
+
+for link in links:
+    if "رزرو غذا" in link.get_attribute("innerHTML"):
+        link.click()
+        break
+
+# label_finds = driver.find_elements(By.XPATH, "//a[@href]")
+# for label_find in label_finds:
+#     print(label_find.get_attribute("innerHTML"))
